e1362ffde6baa23a0a4ca20fb6be9c691bb8da2e
johnTabanyag <143071881+johnTabanyag@users.noreply.github.com>
2024-03-08 21:06:21 +0800

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,31 @@
+let myname = "John Tabanyag";
+let myage = 22; 
+let isStudent = false; 
+
+let num1 = 20;
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+if (myage >= 18) {
+  console.log(myname + " is an adult.");
+} else {
+  console.log(myname + " is a minor.");
+}
+
+if (isStudent) {
+  console.log(myname + " is a student.");
+} else {
+  console.log(myname + " is not a student.");
+}
+
+console.log("Sum:", sum);
+
+console.log("Difference:", difference);
+
+console.log("Product:", product);
+
+console.log("Quotient:", quotient);
